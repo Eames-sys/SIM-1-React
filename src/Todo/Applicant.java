7295f9c94dacade492122350714cfdaf2aa19b09
DELL <eluuemmanuel014@gmail.com>
2025-07-03 09:43:22 +0100

    hello

--- src/Todo/Applicant.java
+++ src/Todo/Applicant.java
@@ -4,4 +4,8 @@ public class Applicant extends Person{
     public Applicant(String name, int age) {
         //super(name,age,id);
     }
+
+    public static void main(String[] args) {
+        Applicant a = new Applicant("Mary", 25);
+    }
 }
--- src/Todo/Course.java
+++ src/Todo/Course.java
@@ -1,12 +1,47 @@
 package Todo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Course {
         private String courseName;
         private String eng;
+        private String math;
+        private Applicant applicant;
         private Teachers teachers;
+        private  String code;
+        private String Title;
 
+    public Course(String Title, String code) {
+    }
 
-    public void setTeachers(Object teachers) {
+    public Course() {
+        
+    }
+
+    public Course(String eng, int i) {
+    }
+
+    public void setTeachers() {
+        this.teachers = (Teachers) teachers;
+    }
+
+    public void add(Course course) {
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+    public void setTitle(String title) {
+        this.Title = title;
+    }
+
+    public String getCode() {
+
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
     }
 }
 
